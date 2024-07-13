3c1735026ddd5fa6a2f3311afa19d0a077da6411
Rui Chen <77432836+sharpchen@users.noreply.github.com>
2024-07-13 13:15:22 +0800

    remove requirement of `sudo`

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Set variables
 url="https://github.com/equalsraf/win32yank/releases/latest/download/win32yank-x64.zip"
 executable="win32yank.exe"
-install_dir="/usr/local/bin"
+install_dir="~/.local/bin"
 
 # Function to display errors and exit
 display_error() {
@@ -22,10 +22,10 @@ wget -q -O "$temp_dir/win32yank.zip" "$url" || display_error "Failed to download
 unzip -q "$temp_dir/win32yank.zip" -d "$temp_dir" || display_error "Failed to unzip $temp_dir/win32yank.zip."
 
 # Move the executable to /usr/local/bin
-sudo mv "$temp_dir/$executable" "$install_dir" || display_error "Failed to move $executable to $install_dir."
+mv "$temp_dir/$executable" "$install_dir" || display_error "Failed to move $executable to $install_dir."
 
 # Grant executable permissions
-sudo chmod +x "$install_dir/$executable" || display_error "Failed to grant executable permissions to $install_dir/$executable."
+chmod +x "$install_dir/$executable" || display_error "Failed to grant executable permissions to $install_dir/$executable."
 
 # Clean up
 rm -rf "$temp_dir" || display_error "Failed to remove temporary directory $temp_dir."
